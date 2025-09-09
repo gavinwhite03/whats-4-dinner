c1a334a3d4d6e4271adc17807ca36ad5038e6c28
gavinwhite03 <anothertexan03@hotmail.com>
2025-09-09 15:53:10 +0200

    Home Page Creation

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,24 @@
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 function Home() {
   return (
-    <div>
-        <button className="Add_Ingredients_Button">Add Ingredients to Your Pantry</button>
-        <button className="Recipes_Button">Find Recipes</button>
+    <div className="home">
+      <section className="intro">
+        <h2>Lets Find Something For Dinner!</h2>
+        <p>What Do You Want to Do?</p>
+      </section>
+
+      <section className="actions">
+        <Link to="/pantry" className="card">
+          <h3>Add Ingredients to Your Pantry</h3>
+        </Link>
+        <Link to="/recipes" className="card">
+          <h3>Find Recipes</h3>
+        </Link>
+      </section>
     </div>
-  )
+  );
 }
+
 export default Home;
